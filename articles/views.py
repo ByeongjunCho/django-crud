85ffadb7a1b54fbc4a1b109c620c42f12b78acd9
ByeongjunCho <jjgk91@naver.com>
2019-08-20 11:20:55 +0900

    07 | articles - CRUD -U (완료)

--- articles/views.py
+++ articles/views.py
@@ -40,4 +40,17 @@ def delete(request, article_pk):
     article.delete()
     return redirect('/articles/')
 
-# def update(request, article_pk):
+def edit(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.title = request.GET.get('title')
+    article.content = request.GET.get('content')
+    article.save()
+
+    return redirect(f'/articles/{article_pk}/')
