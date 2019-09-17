097d3b628c7ec16a7ad477efad3d8eae1e3397c3
ByeongjunCho <jjgk91@naver.com>
2019-09-17 11:45:24 +0900

    17 | Update django form to modelForm

--- articles/forms.py
+++ articles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from .models import Article
 
-class ArticleForm(forms.Form):
+class ArticleForm(forms.ModelForm):
     title = forms.CharField(
         max_length=1, 
         label='제목',
@@ -9,18 +10,43 @@ class ArticleForm(forms.Form):
                 'placeholder': '제목을 입력바랍니다.'
             }
         )
-        )
-    content = forms.CharField(
-        # label 내용 수정
-        label='내용',
-        # Django form에서 HTML 속성 지정 -> widget
-        widget=forms.Textarea(
-            attrs={
-                'class': 'my-content',
-                'placeholder': '내용을 입력바랍니다.',
-                'row': 5,
-                'col': 60
-            }
-        )
     )
+    class Meta:
+        model = Article
+        fields = '__all__'
+        # widgets = {
+        #     'title': forms.TextInput(
+        #         attrs={
+        #             'placeholder': '제목을 입력바랍니다.'
+        #         }
+        #     )
+        # }
+        # fields = ('title', )
+        # exclude = ('title', )
+
+
+
+
+    # title = forms.CharField(
+    #     max_length=1, 
+    #     label='제목',
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'placeholder': '제목을 입력바랍니다.'
+    #         }
+    #     )
+    #     )
+    # content = forms.CharField(
+    #     # label 내용 수정
+    #     label='내용',
+    #     # Django form에서 HTML 속성 지정 -> widget
+    #     widget=forms.Textarea(
+    #         attrs={
+    #             'class': 'my-content',
+    #             'placeholder': '내용을 입력바랍니다.',
+    #             'row': 5,
+    #             'col': 60
+    #         }
+    #     )
+    # )
     
--- w20/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class W20Config(AppConfig):
-    name = 'w20'
